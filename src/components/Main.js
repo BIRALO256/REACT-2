0f4e2d280809a8d4ed83f685a81d5df307a11d4f
MUWANGUZI JOVIC BIRALO <95884686+BIRALO256@users.noreply.github.com>
2024-05-24 19:23:14 +0300

    things i forgot to push in react the other time

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,8 @@
 
-
+import React from "react"
 
 function Main(){
     return(<h1>this is the main section</h1>)
 }
+
+export default Main;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,8 @@
 
+import React from "react"
+
 
 function Nav(){
     return(<p>You are looking at the nav bar</p>)
 }
+export default Nav;
